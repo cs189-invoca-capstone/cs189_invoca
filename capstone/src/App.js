c834ab23f12568b1ebd8ced67ee44ff9c3585b09
Zion Wang <zion@ucsb.umail.edu>
2021-10-19 17:17:11 -0700

    inital stuff lowkey meaningless

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          2021 CS Capstone
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Zion Wang, Adarsh Garg, Koki Narimoto, Shaurye Mahajan, Bryan Xu, Sydney Lim
+        </p>
       </header>
     </div>
   );
