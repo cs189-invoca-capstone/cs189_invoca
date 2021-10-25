3d494903b7082ab60aa0e3cf67823f76f9e81d8a
koki-narimoto <koki.narimoto@gmail.com>
2021-10-25 15:11:38 -0700

    changed naming

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-app.get("/api", (req, res) => {
+app.get("/server", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
   
