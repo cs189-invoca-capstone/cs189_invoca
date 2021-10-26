5d79144849534f6c01d3955c3d672de9554b91d4
Bryan Xu <bryanbobo123@gmail.com>
2021-10-26 14:16:38 -0700

    bx - modified backend and added Procfile

--- server/server.js
+++ server/server.js
@@ -5,6 +5,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../client/public')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/public/index.html'))
+})
+
 app.get("/server", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
