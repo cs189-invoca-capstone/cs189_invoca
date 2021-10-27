24814498f46d1e09e40c40147809ba1400574790
Bryan Xu <bryanbobo123@gmail.com>
2021-10-26 17:14:51 -0700

    bx - updated index.html

--- server/server.js
+++ server/server.js
@@ -10,15 +10,15 @@ const path = require('path')
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '../client/public')))
 
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/public/index.html'))
-})
-
 app.get("/server", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
-  
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+// Anything that doesn't match what's above, send back index.html; 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/public/index.html'))
+})
