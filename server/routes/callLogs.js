f74fba2ff44739a520bf0639fb9d088553e702e1
Adarsh Garg <gargadarsh10@gmail.com>
2021-11-06 18:20:55 -0700

    AG - Finished and tested Put and Delete operations

--- server/routes/callLogs.js
+++ server/routes/callLogs.js
@@ -4,35 +4,12 @@ const User = require("../models/User");
 
 // get all the call logs for that user
 router.get('/all/:userId', async (req, res)=>{
-    try{
-        const user = await User.findById(req.params.userId);
-
-        const allCallLogs = await CallLog.find({})
-
-        let callLogList = []
-        allCallLogs.map((call) => {
-            let {userId} = call;
-            if(userId === req.params.userId){
-                callLogList.push(call);
-            }
-        });
-        res.status(200).json(callLogList);
-    }catch(err){
-        res.status()
-
-        // access too many records at same time, 503
-        // 500 for generic internal server error
-
-        // 400 bad req 
-        // 404 not found 
-        // 401 user is not authorized
-        // 408 time out for request
-    }
+    
 });
 
 // post a call logs that a user wants to upload
 router.post('/', async (req, res)=>{
-
+    
 });
 
 // get a specific call log info
@@ -42,13 +19,43 @@ router.get('/:id', async (req, res)=>{
 
 // update the call log
 router.put('/:id', async (req, res)=>{
+    console.log("Inside Update/Put");
 
+    try {
+        let userId = req.query.userId ? req.query.userId : "";
+        let phoneNumber = req.query.phoneNumber ? req.query.phoneNumber : "";
+        let entireCall = req.query.entireCall ? req.query.entireCall : "";
+        let callSummary = req.query.callSummary ? req.query.callSummary : "";
+        let sentimentAnalysis = req.query.sentimentAnalysis;
+
+        const filter = { _id: req.params.id };
+        const update = { userId: userId, phoneNumber: phoneNumber, entireCall: entireCall, callSummary: callSummary, sentimentAnalysis: sentimentAnalysis };
+        const opts = { new: true };
+        let updatedRecord = await CallLog.findOneAndUpdate(filter, update, opts);
+
+        res.status(200);
+        console.log(updatedRecord);
+    }
+    catch(err){
+        console.log(err);
+    }
 });
 
 
 // delete the call log
 router.delete('/:id', async (req, res)=>{
+    console.log("Inside Delete");
 
+    await CallLog.findOneAndRemove({_id: req.params.id}, (error, deletedRecord) => {
+        if(!error){
+            console.log("Deleted Record: ", deletedRecord);
+            res.status(200);
+        }
+        else{
+            // TODO //
+            console.log(error);
+        }
+    }).clone().catch(function(err){ console.log(err)});
 });
 
 
