775add4c52c7d2c5621dbc9edfb9098653269ca9
Adarsh Garg <gargadarsh10@gmail.com>
2021-11-08 10:58:51 -0800

    Added checks to see if given ID param is in the database

--- server/routes/callLogs.js
+++ server/routes/callLogs.js
@@ -4,7 +4,30 @@ const User = require("../models/User");
 
 // get all the call logs for that user
 router.get('/all/:userId', async (req, res)=>{
-    
+    try{
+        const user = await User.findById(req.params.userId);
+
+        const allCallLogs = await CallLog.find({})
+
+        let callLogList = []
+        allCallLogs.map((call) => {
+            let {userId} = call;
+            if(userId === req.params.userId){
+                callLogList.push(call);
+            }
+        });
+        res.status(200).json(callLogList);
+    }catch(err){
+        res.status()
+
+        // access too many records at same time, 503
+        // 500 for generic internal server error
+
+        // 400 bad req 
+        // 404 not found 
+        // 401 user is not authorized
+        // 408 time out for request
+    }
 });
 
 // post a call logs that a user wants to upload
@@ -22,19 +45,27 @@ router.put('/:id', async (req, res)=>{
     console.log("Inside Update/Put");
 
     try {
-        let userId = req.query.userId ? req.query.userId : "";
-        let phoneNumber = req.query.phoneNumber ? req.query.phoneNumber : "";
-        let entireCall = req.query.entireCall ? req.query.entireCall : "";
-        let callSummary = req.query.callSummary ? req.query.callSummary : "";
-        let sentimentAnalysis = req.query.sentimentAnalysis;
+        await CallLog.exists({ _id: req.params.id }, (error, result) => {
+            if(error || !result){
+                console.log("Error!");
+                throw new Error("ID does not exist in Call Log Database.");
+            }
+            else{
+                let userId = req.query.userId ? req.query.userId : "";
+                let phoneNumber = req.query.phoneNumber ? req.query.phoneNumber : "";
+                let entireCall = req.query.entireCall ? req.query.entireCall : "";
+                let callSummary = req.query.callSummary ? req.query.callSummary : "";
+                let sentimentAnalysis = req.query.sentimentAnalysis;
 
-        const filter = { _id: req.params.id };
-        const update = { userId: userId, phoneNumber: phoneNumber, entireCall: entireCall, callSummary: callSummary, sentimentAnalysis: sentimentAnalysis };
-        const opts = { new: true };
-        let updatedRecord = await CallLog.findOneAndUpdate(filter, update, opts);
+                const filter = { _id: req.params.id };
+                const update = { userId: userId, phoneNumber: phoneNumber, entireCall: entireCall, callSummary: callSummary, sentimentAnalysis: sentimentAnalysis };
+                const opts = { new: true };
+                let updatedRecord = CallLog.findOneAndUpdate(filter, update, opts);
 
-        res.status(200);
-        console.log(updatedRecord);
+                res.status(200);
+                console.log(updatedRecord);
+            }
+        }); // Check if Id exists
     }
     catch(err){
         console.log(err);
@@ -46,16 +77,28 @@ router.put('/:id', async (req, res)=>{
 router.delete('/:id', async (req, res)=>{
     console.log("Inside Delete");
 
-    await CallLog.findOneAndRemove({_id: req.params.id}, (error, deletedRecord) => {
-        if(!error){
-            console.log("Deleted Record: ", deletedRecord);
-            res.status(200);
-        }
-        else{
-            // TODO //
-            console.log(error);
-        }
-    }).clone().catch(function(err){ console.log(err)});
+    try {
+        await CallLog.exists({ _id: req.params.id }, (error, result) => {
+            if(error || !result){
+                console.log("Error!");
+                throw new Error("ID does not exist in Call Log Database.");
+            }
+            else{
+                CallLog.findOneAndRemove({_id: req.params.id}, (err, deletedRecord) => {
+                    if(!err){
+                        console.log("Deleted Record: ", deletedRecord);
+                        res.status(200);
+                    }
+                    else{
+                        throw new Error(err);
+                    }
+                }).clone().catch(function(err){ console.log(err)});
+            }
+        }); // Check if Id exists
+    }
+    catch(err){
+        console.log(err);
+    }
 });
 
 
