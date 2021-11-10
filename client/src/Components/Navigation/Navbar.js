ab3a730927c43d4d4b74962e00adb16f9e0fa24c
Zion Wang <zion@ucsb.umail.edu>
2021-11-10 11:26:33 -0800

    broken changes to navbar

--- client/src/Components/Navigation/Navbar.js
+++ client/src/Components/Navigation/Navbar.js
@@ -1,34 +1,69 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import './Navbar.css'
 import image1 from './index.png'
 import downarrow from './doublearrow.png'
 
-class Navbar extends Component{
-    render (){
-        return (
-            <nav className='NavbarItems'>
-                <div>
-                    <img
-                        src={image1}
-                        height = '60'
-                        alt="Invoca Logo"
-                    />
-                </div>
-                <div class='dropdown'>
-                    <button class="dropbtn">
-                        <div class="dashrect">Dashboard
-                            <div class="weight1"/>
-                            <img src={downarrow} alt="down" class="downarrow"/>
-                        </div>
-                    </button>
-                    <div class="dropdown-content">
-                        <a href="login">Login</a>
-                        <a href="profile">Profile</a>
+const express = require("express");
+
+function Navbar(props) {
+
+    const [profile, setProfile] = useState([])
+    const [loggedin, setLoggedin] = useState(false)
+
+
+
+    if (!loggedin) return (
+        <nav className='NavbarItems'>
+            <div>
+                <img
+                    src={image1}
+                    height = '60'
+                    alt="Invoca Logo"
+                />
+            </div>
+            <div class='dropdown'>
+                <button class="dropbtn">
+                    <div class="dashrect">Dashboard
+                        <div class="weight1"/>
+                        <img src={downarrow} alt="down" class="downarrow"/>
                     </div>
+                </button>
+                <div class="dropdown-content">
+                    <a href="login">Login</a>
+                    <a href="profile">Profile</a>
                 </div>
-            </nav>
-        )
-    }
+            </div>
+        </nav>
+    );
+
+    return (
+        <nav className='NavbarItems'>
+            <div>
+                <img
+                    src={image1}
+                    height = '60'
+                    alt="Invoca Logo"
+                />
+            </div>
+            <div id='circle'>
+                <img src={profile.}>
+                    <a href="profile"/>
+                </img>
+            </div>
+            <div class='dropdown'>
+                <button class="dropbtn">
+                    <div class="dashrect">Dashboard
+                        <div class="weight1"/>
+                        <img src={downarrow} alt="down" class="downarrow"/>
+                    </div>
+                </button>
+                <div class="dropdown-content">
+                    <a href="login">Login</a>
+                    <a href="profile">Profile</a>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
