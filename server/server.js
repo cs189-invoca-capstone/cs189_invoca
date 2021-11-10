1dd3183b4338d87c3ad29234218eba88eff3995b
koki-narimoto <koki.narimoto@gmail.com>
2021-11-09 18:24:23 -0800

    changed port for backend

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use("/callLogs", callLogsRoute);
 app.use("/users", usersRoute);
 
 // listen to port specified
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
