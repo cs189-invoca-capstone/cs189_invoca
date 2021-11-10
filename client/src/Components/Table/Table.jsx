09f811cd09ef28c455103d6dcd64219278d37dd9
Bryan Xu <bryanbobo123@gmail.com>
2021-11-09 18:00:18 -0800

    bx - added modal popup to table

--- client/src/Components/Table/Table.jsx
+++ client/src/Components/Table/Table.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-import ".././Style.css";
+// import ".././Style.css";
 import "./Table.css";
 import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal'
@@ -15,7 +15,7 @@ export default function Table() {
     const handleClose = () => setShow(false);
 
     const data = [
-        {callerID: "Koki Narimoto", phone: "(660) 660-6600", summary: "Team Lead duties. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", status: "Pending"},
+        {callerID: "Koki Narimoto", phone: "(660) 660-6600", summary: "Team Lead duties. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", status: "Pending"},
         {callerID: "Bryan Xu", phone: "(408) 408-4080", summary: "Duties? hehe", status: "In progress"},
         {callerID: "Adarsh Garg", phone: "(510) 510-5101", summary: "Big changes", status: "Fixed"},
         {callerID: "Shaurye Mahajan", phone: "(626) 626-6262", summary: "Small steps", status: "Completed"},
@@ -28,13 +28,22 @@ export default function Table() {
         setTableData(data)
     }
 
+    const handleDelete = (data) => {
+        console.log("delete button pressed for caller: " + data.callerID);
+    }
+
     const renderData = (data, index) => {
         return(
-            <tr key={index} onClick={() => handleTableClick(data)}>
-                <td>{data.callerID}</td>
-                <td>{data.phone}</td>
-                <td  className='tableStyle'>{data.summary}</td>
-                <td>{data.status}</td>
+            <tr key={index}>
+                <td onClick={() => handleTableClick(data)}>{data.callerID}</td>
+                <td onClick={() => handleTableClick(data)}>{data.phone}</td>
+                <td onClick={() => handleTableClick(data)} className='tableStyle'>{data.summary}</td>
+                <td onClick={() => handleTableClick(data)}>{data.status}</td>
+                <td onClick={() => handleDelete(data)}>    
+                <Button variant="danger">
+                    Delete
+                </Button>
+                </td>
             </tr>
         )
     }
@@ -50,6 +59,7 @@ export default function Table() {
                     <th>Phone Number</th>
                     <th  data-testid="summary-table">Summary</th>
                     <th>Status</th>
+                    <th>Delete?</th>
                 </tr>
             </thead>
             <tbody>
@@ -59,15 +69,15 @@ export default function Table() {
 
         <Modal show={show} onHide={handleClose} info={tableData}>
             <Modal.Header closeButton>
-            <Modal.Title>{tableData.callerID} {tableData.phone}</Modal.Title>
+            <Modal.Title><h4>{tableData.callerID}: {tableData.phone}</h4></Modal.Title>
             </Modal.Header>
             <Modal.Body>{tableData.summary}</Modal.Body>
             <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-                Close
-            </Button>
             <Button variant="primary" onClick={handleClose}>
                 Edit
+            </Button>
+            <Button variant="secondary" onClick={handleClose}>
+                Close
             </Button>
             </Modal.Footer>
         </Modal>
