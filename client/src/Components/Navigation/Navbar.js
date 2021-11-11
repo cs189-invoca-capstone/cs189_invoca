9a55fe769804c403d6a412d33cfd6cf20de87e6b
Zion Wang <zion@ucsb.umail.edu>
2021-11-11 13:28:33 -0800

    broken pull for the login page for the front end

--- client/src/Components/Navigation/Navbar.js
+++ client/src/Components/Navigation/Navbar.js
@@ -2,39 +2,58 @@ import React, {Component, useEffect, useState} from 'react';
 import './Navbar.css'
 import image1 from './index.png'
 import downarrow from './doublearrow.png'
+import axios from 'axios'
 
-const express = require("express");
+async function loginPage() {
+    axios.get('/login').then(res => {
+        console.log(res)
+     });
+}
 
 function Navbar(props) {
 
     const [profile, setProfile] = useState([])
     const [loggedin, setLoggedin] = useState(false)
 
+    // useEffect(() => {
+    //     const getProfile = async () => {
+    //         try{
+    //             // swap 0 with actual call id once we extract it from login
+    //             // and once the databse actually stores the user ID
+    //             const temp = await axios("User/email");
+    //             console.log(temp);
+    //             setProfile(temp);
+    //         }catch(err){
+    //             console.log(err);
+    //         }
+    //     };
+    //     getProfile();
+    //     setLoggedin(true)
+    // }, [])
 
-
-    if (!loggedin) return (
-        <nav className='NavbarItems'>
-            <div>
-                <img
-                    src={image1}
-                    height = '60'
-                    alt="Invoca Logo"
-                />
-            </div>
-            <div class='dropdown'>
-                <button class="dropbtn">
-                    <div class="dashrect">Dashboard
-                        <div class="weight1"/>
-                        <img src={downarrow} alt="down" class="downarrow"/>
-                    </div>
-                </button>
-                <div class="dropdown-content">
-                    <a href="login">Login</a>
-                    <a href="profile">Profile</a>
-                </div>
-            </div>
-        </nav>
-    );
+    // if (!loggedin) return (
+    //     <nav className='NavbarItems'>
+    //         <div>
+    //             <img
+    //                 src={image1}
+    //                 height = '60'
+    //                 alt="Invoca Logo"
+    //             />
+    //         </div>
+    //         <div class='dropdown'>
+    //             <button class="dropbtn">
+    //                 <div class="dashrect">Dashboard
+    //                     <div class="weight1"/>
+    //                     <img src={downarrow} alt="down" class="downarrow"/>
+    //                 </div>
+    //             </button>
+    //             <div class="dropdown-content">
+    //                 <a href="login">Login</a>
+    //                 <a href="profile">Profile</a>
+    //             </div>
+    //         </div>
+    //     </nav>
+    // );
 
     return (
         <nav className='NavbarItems'>
@@ -46,9 +65,7 @@ function Navbar(props) {
                 />
             </div>
             <div id='circle'>
-                <img src={profile.}>
-                    <a href="profile"/>
-                </img>
+                <a href="profile">{profile}</a>
             </div>
             <div class='dropdown'>
                 <button class="dropbtn">
@@ -58,7 +75,7 @@ function Navbar(props) {
                     </div>
                 </button>
                 <div class="dropdown-content">
-                    <a href="login">Login</a>
+                    <button onClick= {() =>loginPage()}>Login</button>
                     <a href="profile">Profile</a>
                 </div>
             </div>
