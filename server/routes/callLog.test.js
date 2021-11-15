a75d7496b047432687dbc494e0a8bf0506c56d2c
Shaurye Mahajan <shaurye@ucsb.edu>
2021-11-15 12:59:18 -0800

    sm - search feature implmeneted, testing with edge cases for endpoints,

--- server/routes/callLog.test.js
+++ server/routes/callLog.test.js
@@ -5,11 +5,11 @@ const app = require('../server.js')
 describe('Post Endpoints', () => {
   it('should post a new call', async () => {
     const res = await request(app).post('/callLogs/')
-    .send({userId: 4})
+    .send({userId: 4, phoneNumber: "4154727981", entireCall: "This is the entire call", callSummary: "call summary", sentimentAnalysis: "false"})
     expect(res.text).toEqual("Data Inserted")
 
     const res2 = await request(app).post('/callLogs/')
-    .send({userId: 4})
+    .send({userId: 4, phoneNumber: "4158909000", entireCall: "This is the entire call 2", callSummary: "call summary 2", sentimentAnalysis: "false"})
     expect(res2.text).toEqual("Data Inserted")
 
   })
@@ -19,10 +19,31 @@ describe('Post Endpoints', () => {
 
 describe('Post Endpoints Edge', () => {
   it('should return error with no UserId', async () => {
-    const res = await request(app).post('/callLogs/')
+
+    const missingID = await request(app).post('/callLogs/')
     .send()
-    expect(res.text).toEqual("No UserId specified")
-    expect(res.status).toEqual(400)
+    expect(missingID.text).toEqual("Missing a required parameter")
+    expect(missingID.status).toEqual(400)
+
+    const missingPhone = await request(app).post('/callLogs/')
+    .send({userId: 4, entireCall: "This is the entire call 2", callSummary: "call summary 2", sentimentAnalysis: "false"})
+    expect(missingPhone.text).toEqual("Missing a required parameter")
+    expect(missingPhone.status).toEqual(400)
+
+    const missingEntire = await request(app).post('/callLogs/')
+    .send({userId: 4, phoneNumber: "4158909000", callSummary: "call summary 2", sentimentAnalysis: "false"})
+    expect(missingEntire.text).toEqual("Missing a required parameter")
+    expect(missingEntire.status).toEqual(400)
+
+    const missingSummary = await request(app).post('/callLogs/')
+    .send({userId: 4, phoneNumber: "4158909000", entireCall: "This is the entire call 2", sentimentAnalysis: "false"})
+    expect(missingSummary.text).toEqual("Missing a required parameter")
+    expect(missingSummary.status).toEqual(400)
+
+    const missingSentiment = await request(app).post('/callLogs/')
+    .send({userId: 4, phoneNumber: "4158909000", entireCall: "This is the entire call 2", callSummary: "call summary 2"})
+    expect(missingSentiment.text).toEqual("Missing a required parameter")
+    expect(missingSentiment.status).toEqual(400)
 
   })
 
@@ -57,6 +78,55 @@ describe('Get Endpoints Edge', () => {
   it('should error on get', async () => {
     const res2 = await request(app).get('/callLogs/test')
     expect(res2.status).toEqual(400)
+  })
+
+})
+
+describe('Search Endpoint', () => {
+
+  it('should return calls with given query', async () => {
+    const res = await request(app).get('/callLogs/search/').send({"searchType": "phoneNumber", "searchQuery": "415890"})
+    expect(res.status).toEqual(200)
+    for(i in res.body){
+      
+      expect(res.body[i].phoneNumber).toMatch(/415890/);
+    
+    }
+  })
+
+  it('should return calls with given query', async () => {
+    const res2 = await request(app).get('/callLogs/search/').send({"searchType": "entireCall", "searchQuery": "2"})
+    expect(res2.status).toEqual(200)
+    for(i in res2.body){
+      
+      expect(res2.body[i].entireCall).toMatch(/2/);
+    
+    }
+  })
+
+})
+
+describe('Search Endpoint Edge', () => {
+
+  it('should return an error', async () => {
+    const res = await request(app).get('/callLogs/search/').send({"searchQuery": "415890"})
+    expect(res.status).toEqual(400)
+    expect(res.text).toEqual("Missing Parameters")
+    console.log(res)
+  })
+
+  it('should return an error', async () => {
+    const res2 = await request(app).get('/callLogs/search/').send({"searchType": "entireCall"})
+    expect(res2.text).toEqual("Missing Parameters")
+    expect(res2.status).toEqual(400)
+
+  })
+
+  it('should return an error', async () => {
+    const res3 = await request(app).get('/callLogs/search/').send({"searchType": "wrongParam", "searchQuery": "2"})
+    expect(res3.status).toEqual(400)
+    expect(res3.text).toEqual("Search Type not valid")
+   
   })
 
 })
