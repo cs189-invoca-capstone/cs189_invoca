5674fe856d7467010e9ab8d3ece358a9182456ef
Zion Wang <zion@ucsb.umail.edu>
2021-11-15 14:29:16 -0800

    added the pages and a new landing page with all the link currently working

---
+++ client/src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React, {Component, useEffect, useState} from 'react';
+import './LandingPage.css'
+import Navbar from '../Navigation/Navbar'
+
+function LandingPage(){
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    );
+}
+
+export default LandingPage;
